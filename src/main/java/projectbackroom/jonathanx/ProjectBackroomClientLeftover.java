5a60461791a5f28dca70a7672e078d3776206f0a
jonathanpecany <jonjam1208@gmail.com>
2024-03-20 19:21:37 -0400

    Tried adding some random lighting to dropceiling lights.

--- src/main/java/projectbackroom/jonathanx/ProjectBackroomClientLeftover.java
+++ src/main/java/projectbackroom/jonathanx/ProjectBackroomClientLeftover.java
@@ -2,7 +2,7 @@ package projectbackroom.jonathanx;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ProjectBackroomClient implements ClientModInitializer {
+public class ProjectBackroomClientLeftover implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
--- src/main/java/projectbackroom/jonathanx/blocks/moddedBlocks.java
+++ src/main/java/projectbackroom/jonathanx/blocks/moddedBlocks.java
@@ -1,14 +1,19 @@
 package projectbackroom.jonathanx.blocks;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import projectbackroom.jonathanx.ProjectBackroom;
 import projectbackroom.jonathanx.sound.moddedSounds;
@@ -25,12 +30,23 @@ public class moddedBlocks {
                     SoundEvent.of(Identifier.tryParse("minecraft:block.wool.hit")),
                     SoundEvent.of(Identifier.tryParse("minecraft:block.wool.fall"))
             )
-            // break
-            // step
-            // place
-            // hit
-            // fall
     )));
+    public static final Block LEVEL_0_DROPCEILING = registerBlock("level_0_dropceiling", new Block(FabricBlockSettings.create()));
+    public static final Block LEVEL_0_LIGHT = registerBlock("level_0_led_dropceiling", new level_0_led_dropceiling(AbstractBlock.Settings.create()));
+    //new Block(FabricBlockSettings.create().luminance(8).sounds(BlockSoundGroup.GLASS))
+
+    public static final ItemGroup BACKROOM_BLOCKS = FabricItemGroup.builder()
+            .displayName(Text.translatable("itemgroup.project_backroom.backroom_blocks"))
+            .icon(() -> new ItemStack(LEVEL_0_WALLPAPER.asItem()))
+            .entries(((displayContext, entries) -> {
+                entries.add(LEVEL_0_WALLPAPER);
+                entries.add(LEVEL_0_DROPCEILING);
+                entries.add(LEVEL_0_LIGHT);
+                entries.add(LEVEL_0_YELLOW_CARPET);
+            }))
+            .build();
+
+    public static ItemGroup itemGroupRegistry = Registry.register(Registries.ITEM_GROUP, new Identifier(ProjectBackroom.MOD_ID,"backroom_blocks"), BACKROOM_BLOCKS);
 
     public static Block registerBlock(String name, Block block){
         registerBlockItem(name,block);
@@ -38,7 +54,8 @@ public class moddedBlocks {
     }
 
     public static Item registerBlockItem(String name, Block block){
-        return Registry.register(Registries.ITEM, new Identifier(ProjectBackroom.MOD_ID,name), new BlockItem(block,new FabricItemSettings()));
+        Item ITEM = Registry.register(Registries.ITEM, new Identifier(ProjectBackroom.MOD_ID,name), new BlockItem(block,new FabricItemSettings()));
+        return ITEM;
     }
 
     public static void registerModdedBlocks(){
